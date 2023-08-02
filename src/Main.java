b33f56324a593dd26074654f54107a4be8125c61
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-08-01 22:52:57 -0400

    build: :tada: init repository

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(++x);
+        ++x;
+        x++;
+        System.out.println(++x);
+        System.out.println(x++);
+        System.out.println(++x);
+
+    }
+
+}
